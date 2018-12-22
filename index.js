3de59c4df538e2dc08113558f6132cdae4561700
Thomas GENTILHOMME <gentilhomme.thomas@gmail.com>
2018-12-22 16:38:49 +0100

    refactor: remove find-proc and execute a Node.js spawn

--- index.js
+++ index.js
@@ -1,19 +1,60 @@
-// Require Third-party Dependencies
-const fp = require("find-process");
+// Require Node.js Dependencies
+const { spawn } = require("child_process");
 
 /**
- * @async
+ * @generator
+ * @func split
+ * @param {!Buffer} buf buffer
+ * @param {Number} [code] charCode
+ * @returns {IterableIterator<Buffer>}
+ */
+function* split(buf, code = "\n".charCodeAt(0)) {
+    let index;
+    let offset = 0;
+    while ((index = buf.indexOf(code, offset)) !== -1) {
+        yield buf.slice(offset, index);
+        offset = index + 1;
+    }
+    yield buf.slice(offset);
+}
+
+/**
  * @func isPowershell
  * @desc Known if the process parent id is powershell
- * @returns {Boolean}
+ * @returns {Promise<Boolean>}
  */
-async function isPowershell() {
-    const result = await fp("pid", process.ppid);
-    if (result.length === 0) {
-        return false;
-    }
+function isPowershell() {
+    return new Promise((resolve, reject) => {
+        if (process.platform !== "win32") {
+            return reject(new Error("Cannot execute the method, process.platform must be win32!"));
+        }
 
-    return result[0].name === "powershell.exe";
+        const ppid = Buffer.from(process.ppid.toString());
+        const pname = Buffer.from("powershell.exe");
+
+        const cp = spawn("cmd", ["/c", "WMIC path win32_process get Name,Processid"]);
+
+        /** @type {Buffer[]} */
+        const stdBuf = [];
+        cp.stdout.on("data", (buf) => stdBuf.push(buf));
+
+        cp.on("close", (code) => {
+            if (code !== 0) {
+                return reject(new Error(`Command terminated with error code ${code}`));
+            }
+
+            const lines = Buffer.concat(stdBuf);
+            for (const buf of split(lines)) {
+                if (buf.indexOf(ppid) !== -1 && buf.indexOf(pname) === 0) {
+                    return resolve(true);
+                }
+            }
+
+            return resolve(false);
+        });
+
+        return void 0;
+    });
 }
 
 module.exports = isPowershell;
